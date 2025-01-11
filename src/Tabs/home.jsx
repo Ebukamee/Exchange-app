f86fcb66614694a098eed5501274c3be2396f8fc
Ebuka <chukwuebukanwokike2007@gmail.com>
2025-01-11 14:37:49 +0100

    dashboard home tab

--- src/Tabs/home.jsx
+++ src/Tabs/home.jsx
@@ -1,6 +1,8 @@
-import { Box, Text, Heading, Flex,Image } from "@chakra-ui/react";
+import { Box, Text, Heading, Flex,Image, Container } from "@chakra-ui/react";
 import { Blue } from "../assets/Colors";
 import eye from '../assets/images/eye.svg'
+import Amazon from '../assets/images/giftcard.svg'
+import Bitcoin from '../assets/images/bitcoin.png'
 
 export default function Home() {
   return (
@@ -13,7 +15,7 @@ export default function Home() {
           Chukwuebuka
         </Text>
       </Box>
-      <Box bg={Blue.p} color="white" p={5} rounded="md" mb={10}>
+      <Box bg={Blue.p} color="white" p={7} rounded="md" mb={10}>
         <Flex justifyContent='space-between'fontSize="12px">
           <Text color="gray.200">Total Trasanction Value</Text>
           <Image src={eye} alt='peep eye' />
@@ -25,17 +27,33 @@ export default function Home() {
           0.00
         </Heading>
       </Box>
-      <Box>
+      <Container maxW='700px'>
         <Text fontSize='sm' color='gray.700' >
             Quick Actions
         </Text>
-        <Box my={5} p={5} rounded='md' bg={Blue.bg}>
-            <Heading as='p' fontSize='xl'>Trade Giftcards</Heading>
+        <Box my={5} p={5} rounded='lg' bg="orange.100" maxW='700px'>
+            <Flex justifyContent="space-between" alignItems='center'color='gray.500'>
+                <Box>
+                <Heading as='p' fontSize='lg' mb={4}>Trade Giftcards</Heading>
+                <Text fontSize='sm'>
+                      Exchange your Giftcards and receive instant cash
+                    </Text> 
+                </Box>
+                <Image src={Amazon} alt="giftcard" w="100px" />
+            </Flex>
         </Box>
-        <Box my={5} rounded="md">
-
+        <Box my={5} p={5} rounded='lg' bg={Blue.bg} maxW='700px'>
+            <Flex justifyContent="space-between" alignItems='center' color='gray.500'>
+                <Box>
+                <Heading as='p' fontSize='lg' mb={4}>Trade Crypto</Heading>
+                <Text fontSize='sm'>
+                      Send in Cryptocurrencies to our wallet and recieve your money worth instantly 
+                    </Text> 
+                </Box>
+                <Image src={Bitcoin} alt="giftcard" w="100px" />
+            </Flex>
         </Box>
-      </Box>
+      </Container>
     </Box>
   );
 }
