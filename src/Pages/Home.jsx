6475581bd456aa65aaa24627546efe6faafba582
Ebuka <99645482+Ebukamee@users.noreply.github.com>
2025-08-30 16:30:23 +0100

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import Nav from '../components/nav'
 import Hero from '../components/Hero'
-import TestimonialsSection from '../components/Testiomonials.jsx'
+import TestimonialsSection from '../components/Testimonials.jsx'
 import FeaturesSection from '../components/feature'
 import Other from '../components/othersection'
 import FAQ from '../components/FAQ'
